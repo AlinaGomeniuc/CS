2241a62ccf352f418151e22de47a0808e98dc3db
Alina Gomeniuc <agomeniuc@gmail.com>
2020-11-28 16:57:55 +0200

    added report

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"."}
+        glue = {"."},
+        plugin = {"json:target/cucumber.json", "pretty","html:target/cucumber-reports"}
 )
 
 
